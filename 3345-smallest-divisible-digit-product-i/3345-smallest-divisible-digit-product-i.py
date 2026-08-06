50c1e5b4a1de3b311a14aaece32f586edd061205
TaNaY-GaLa <tanaygala23oct@gmail.com>
2026-08-06 22:58:59 +0530

    Time: 0 ms (100%), Space: 12.4 MB (62.03%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def smallestNumber(self, n, t):
+        while True:
+            x = n
+            prod = 1
+            if x == 0:
+                prod = 0
+            else:
+                while x > 0:
+                    prod *= x % 10
+                    x //= 10
+            if prod % t == 0:
+                return n
+            n += 1
